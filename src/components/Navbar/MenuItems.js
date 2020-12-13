fcd8165fc8952abb690b94431a8b8babe2dd8cd1
Ishak Firdauzi Ruslan <ishak.firdauzi@gmail.com>
2020-12-13 13:58:41 +0700

    diarium-lite-0.0.1

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'My Profile',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Help',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Log Out',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { MenuItems } from "./MenuItems"
+import { Button } from "../Button"
+import { Logo } from "../Diarium.png"
+import './Navbar.css'
+
+class Navbar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
+    render() {
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">
+                <img className="diarium_logo" src={ Logo } />Diarium Lite<i className="fab"></i></h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <Button>Log Out</Button>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
